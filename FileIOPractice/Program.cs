df0190a7cf482082585196544af9065ba713f0b5
алина человечкова <alinasmart19@mail.ru>
2021-02-18 18:58:18 +0600

    Добавьте файлы проекта.

---
+++ FileIOPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Путь по умолчанию: D:\\c#Homework\\information.txt");
+
+            Console.Write("Введите полный путь: ");
+            string writePath = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(writePath))
+            {
+                writePath = @"D:\c#Homework\information.txt";
+            }
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+
+            
+
+            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(name);
+                    sw.WriteLine(surname);
+                    sw.WriteLine(age);
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
